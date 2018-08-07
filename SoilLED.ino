c35d4f1fe01b81d49d3faafbda3862d49204c747
ttgray <41284128+ttgray@users.noreply.github.com>
2018-08-07 16:31:07 +0930

    Add files via upload

---
+++ SoilLED.ino
@@ -0,0 +1,20 @@
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  int val;
+  val = analogRead(0); 
+  if ( val <= 300 )
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  Serial.println(val);
+  delay(1000);
+}
+
